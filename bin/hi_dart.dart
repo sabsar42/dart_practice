5f6d9b917f1f65cc1b495bdb37fec876807b7011
Shakib Absar <sabsar42@gmail.com>
2023-08-03 00:48:36 +0600

    Assignment - 1

---
+++ bin/hi_dart.dart
@@ -0,0 +1,6 @@
+
+void main(){
+ print('hello');
+ print('hello');
+ print('hello');
+}
---
+++ bin/variable.dart
@@ -0,0 +1,23 @@
+import 'dart:io';
+
+void main(){
+
+  // Numeric = integer, floating
+  // Textual = String
+  // Boolean = True/False
+  // variable = container,
+  int age = 45 ;
+  print(age);
+  double cgpa = 2.00;
+  print(2+cgpa);
+
+   String? name = stdin.readLineSync();
+  String s = 'Shakib';
+  int a = 10 ;
+  String det = 'Full name : $s Age : $a';
+  print(det);
+  print(name);
+
+
+
+}
